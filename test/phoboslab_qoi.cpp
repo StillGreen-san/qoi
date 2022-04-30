ca24d4257e17da2197b38533f263205fdc9a8b3e
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2022-04-30 12:52:43 +0200

    impl phoboslab_qoi & shadowmitia_libqoi functions

--- test/phoboslab_qoi.cpp
+++ test/phoboslab_qoi.cpp
@@ -1,21 +1,93 @@
-
 #include "qoiimpls.hpp"
 
-//#define QOI_IMPLEMENTATION
 #include <qoi.h>
 
-#include <memory>
+namespace impl::phoboslab
+{
+struct Decoder : public IImageData
+{
+	Decoder() = delete;
+	Decoder(const std::vector<uint8_t>& qoi) : pixels{::qoi_decode(qoi.data(), qoi.size(), &description, 0)}
+	{
+	}
+	const uint8_t* data() override
+	{
+		return reinterpret_cast<const uint8_t*>(pixels);
+	}
+	size_t size() override
+	{
+		return width() * height() * channels();
+	}
+	uint32_t width() override
+	{
+		return description.width;
+	}
+	uint32_t height() override
+	{
+		return description.height;
+	}
+	uint8_t channels() override
+	{
+		return description.channels;
+	}
+	uint8_t colorspace() override
+	{
+		return description.colorspace;
+	}
+	~Decoder() override
+	{
+		free(pixels);
+	}
+	::qoi_desc description;
+	void* pixels;
+};
+struct Encoder : public IImageData
+{
+	Encoder() = delete;
+	Encoder(const std::vector<uint8_t>& raw, ImageDescription desc)
+	{
+		description.width = desc.width;
+		description.height = desc.height;
+		description.channels = desc.channels;
+		description.colorspace = desc.colorspace;
+		pixels = ::qoi_encode(raw.data(), &description, &length);
+	}
+	const uint8_t* data() override
+	{
+		return reinterpret_cast<const uint8_t*>(pixels);
+	}
+	size_t size() override
+	{
+		return length;
+	}
+	uint32_t width() override
+	{
+		return description.width;
+	}
+	uint32_t height() override
+	{
+		return description.height;
+	}
+	uint8_t channels() override
+	{
+		return description.channels;
+	}
+	uint8_t colorspace() override
+	{
+		return description.colorspace;
+	}
+	::qoi_desc description;
+	void* pixels;
+	int length;
+};
 
-void encode()
+std::unique_ptr<IImageData> decode(const std::vector<uint8_t>& qoi)
 {
-	::qoi_desc desc;
-	int size;
-	void* data = ::qoi_encode(nullptr, &desc, &size);
-	free(data);
+	return std::make_unique<Decoder>(qoi);
 }
-void decode()
+
+std::unique_ptr<IImageData> encode(const std::vector<uint8_t>& raw, const ImageDescription& desc)
 {
-	::qoi_desc desc;
-	void* data = ::qoi_decode(nullptr, 0, &desc, 4);
-	free(data);
+	return std::make_unique<Encoder>(raw, desc);
 }
+} // namespace impl::phoboslab
--- test/shadowmitia_libqoi.cpp
+++ test/shadowmitia_libqoi.cpp
@@ -2,15 +2,83 @@
 
 #include <qoi.hpp>
 
-void encode()
+namespace impl::shadowmitia
 {
-	std::vector<unsigned char> pix;
-	auto data = qoi::encode(pix, 0, 0, 4);
-}
-void decode()
+struct Decoder : public IImageData
 {
-	std::vector<unsigned char> data;
-	auto pix = qoi::decode(data);
-	bool vld = qoi::is_valid(data);
-	auto desc = qoi::get_header(data);
+	Decoder() = delete;
+	Decoder(const std::vector<uint8_t>& qoi) : pixels{qoi::decode(qoi)}, header{qoi::get_header(qoi)}
+	{
+	}
+	const uint8_t* data() override
+	{
+		return pixels.data();
+	}
+	size_t size() override
+	{
+		return pixels.size();
+	}
+	uint32_t width() override
+	{
+		return header.width;
+	}
+	uint32_t height() override
+	{
+		return header.height;
+	}
+	uint8_t channels() override
+	{
+		return header.channels;
+	}
+	uint8_t colorspace() override
+	{
+		return header.colorspace;
+	}
+	std::vector<uint8_t> pixels;
+	qoi::header header;
+};
+struct Encoder : public IImageData
+{
+	Encoder() = delete;
+	Encoder(const std::vector<uint8_t>& raw, ImageDescription desc) :
+	    pixels{qoi::encode(raw, desc.width, desc.height, desc.channels)}, header{qoi::get_header(pixels)}
+	{
+	}
+	const uint8_t* data() override
+	{
+		return pixels.data();
+	}
+	size_t size() override
+	{
+		return pixels.size();
+	}
+	uint32_t width() override
+	{
+		return header.width;
+	}
+	uint32_t height() override
+	{
+		return header.height;
+	}
+	uint8_t channels() override
+	{
+		return header.channels;
+	}
+	uint8_t colorspace() override
+	{
+		return header.colorspace;
+	}
+	std::vector<uint8_t> pixels;
+	qoi::header header;
+};
+
+std::unique_ptr<IImageData> decode(const std::vector<uint8_t>& qoi)
+{
+	return std::make_unique<Decoder>(qoi);
 }
+
+std::unique_ptr<IImageData> encode(const std::vector<uint8_t>& raw, const ImageDescription& desc)
+{
+	return std::make_unique<Encoder>(raw, desc);
+}
+} // namespace impl::shadowmitia
