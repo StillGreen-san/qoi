b3eadfebffa9d9dac02e5116b544aacf9ec88ed5
StillGreen-san <40620628+StillGreen-san@users.noreply.github.com>
2023-07-21 21:30:29 +0200

    refactor project options to only apply to select targets

--- cmake/ProjectSettings.cmake
+++ cmake/ProjectSettings.cmake
@@ -1,29 +1,42 @@
 # based on https://github.com/cpp-best-practices/cmake_template
 
-# treat the value of the CXX_STANDARD target property as a requirement
-set( CMAKE_CXX_STANDARD_REQUIRED ON )
-
 # generate compile_commands.json to make it easier to work with clang based tools
 set( CMAKE_EXPORT_COMPILE_COMMANDS ON )
 
-if( ENABLE_IPO )
-    # enable link time optimization
-    include( CheckIPOSupported )
-    check_ipo_supported( RESULT ipo_supported OUTPUT ipo_output )
-    if( ipo_supported )
-        set( CMAKE_INTERPROCEDURAL_OPTIMIZATION ON )
-    else( )
-        message( ERROR "IPO is not supported: ${ipo_output}" )
-    endif( )
-endif( )
+function(target_try_enable_ipo target)
+	if( ENABLE_IPO )
+		# enable link time optimization
+		include( CheckIPOSupported )
+		check_ipo_supported( RESULT ipo_supported OUTPUT ipo_output )
+		if( ipo_supported )
+			set_property( TARGET ${target} PROPERTY INTERPROCEDURAL_OPTIMIZATION ON )
+		else( )
+			message( ERROR "IPO is not supported: ${ipo_output}" )
+		endif( )
+	endif( )
+endfunction()
+
+include( ${CMAKE_CURRENT_SOURCE_DIR}/cmake/StaticAnalyzers.cmake )
+include( ${CMAKE_CURRENT_SOURCE_DIR}/cmake/CompilerWarnings.cmake )
 
 # Enhance error reporting and compiler messages
 if( CMAKE_CXX_COMPILER_ID MATCHES ".*Clang" )
-    add_compile_options( -fcolor-diagnostics )
+	add_compile_options( -fcolor-diagnostics )
 elseif( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
-    add_compile_options( -fdiagnostics-color=always )
+	add_compile_options( -fdiagnostics-color=always )
 elseif( CMAKE_CXX_COMPILER_ID STREQUAL "MSVC" AND MSVC_VERSION GREATER 1900 )
-    add_compile_options( /diagnostics:column )
+	add_compile_options( /diagnostics:column )
 else( )
-    message( STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler." )
+	message( STATUS "No colored compiler diagnostic set for '${CMAKE_CXX_COMPILER_ID}' compiler." )
 endif( )
+
+macro( add_project_option_interface target )
+	add_library( ${target} INTERFACE )
+	# treat the value of the CXX_STANDARD target property as a requirement
+	set_property( TARGET ${target} PROPERTY CXX_STANDARD_REQUIRED ON )
+	target_try_enable_include_what_you_use( ${target} )
+	target_try_enable_ipo( ${target} )
+	target_try_enable_clang_tidy( ${target} )
+	target_try_enable_cppcheck( ${target} )
+	target_try_enable_compiler_warnings( ${target} )
+endmacro( )
--- cmake/StaticAnalyzers.cmake
+++ cmake/StaticAnalyzers.cmake
@@ -1,62 +1,68 @@
 # based on https://github.com/cpp-best-practices/cmake_template
 
-if( ENABLE_CPPCHECK )
-    find_program( CPPCHECK cppcheck )
-    if( CPPCHECK )
-		if( CMAKE_GENERATOR MATCHES ".*Visual Studio.*" )
-			set( CPPCHECK_TEMPLATE "vs" )
-		else( )
-			set( CPPCHECK_TEMPLATE "gcc" )
-		endif( )
-        set( CMAKE_CXX_CPPCHECK
-			 ${CPPCHECK}
-			 --template=${CPPCHECK_TEMPLATE}
-			 --enable=style,performance,warning,portability
-			 --inline-suppr
-			 # We cannot act on a bug/missing feature of cppcheck
-			 --suppress=cppcheckError
-			 --suppress=internalAstError
-			 # if a file does not have an internalAstError, we get an unmatchedSuppression error
-			 --suppress=unmatchedSuppression
-			 # noisy and incorrect sometimes
-			 --suppress=passedByValue
-			 # ignores code that cppcheck thinks is invalid C++
-			 --suppress=syntaxError
-			 --suppress=preprocessorErrorDirective
-			 --inconclusive
-            )
-    else( )
-        message( SEND_ERROR "cppcheck requested but executable not found" )
-    endif( )
-endif( )
-
-if( ENABLE_CLANG_TIDY )
-    find_program( CLANGTIDY clang-tidy )
-    if( CLANGTIDY )
-        set( CMAKE_CXX_CLANG_TIDY
-             ${CLANGTIDY}
-			 -extra-arg=-Wno-unknown-warning-option
-			 -extra-arg=-Wno-ignored-optimization-argument
-			 -extra-arg=-Wno-unused-command-line-argument
-			 -p
-        )
-		if( NOT "${CMAKE_CXX_STANDARD}" STREQUAL "" )
-			if( "${CLANG_TIDY_OPTIONS_DRIVER_MODE}" STREQUAL "cl" )
-				set( CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=/std:c++${CMAKE_CXX_STANDARD} )
+function( target_try_enable_cppcheck target )
+	if( ENABLE_CPPCHECK )
+		find_program( CPPCHECK cppcheck )
+		if( CPPCHECK )
+			if( CMAKE_GENERATOR MATCHES ".*Visual Studio.*" )
+				set( CPPCHECK_TEMPLATE "vs" )
 			else( )
-				set(CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=-std=c++${CMAKE_CXX_STANDARD} )
+				set( CPPCHECK_TEMPLATE "gcc" )
 			endif( )
+			set( CPPCHECK_OPTIONS
+				 ${CPPCHECK}
+				 --template=${CPPCHECK_TEMPLATE}
+				 --enable=style,performance,warning,portability
+				 --inline-suppr
+				 # We cannot act on a bug/missing feature of cppcheck
+				 --suppress=cppcheckError
+				 --suppress=internalAstError
+				 # if a file does not have an internalAstError, we get an unmatchedSuppression error
+				 --suppress=unmatchedSuppression
+				 # noisy and incorrect sometimes
+				 --suppress=passedByValue
+				 # ignores code that cppcheck thinks is invalid C++
+				 --suppress=syntaxError
+				 --suppress=preprocessorErrorDirective
+				 --inconclusive )
+			set_property( TARGET ${target} PROPERTY CXX_CPPCHECK ${CPPCHECK_OPTIONS} )
+		else( )
+			message( SEND_ERROR "cppcheck requested but executable not found" )
 		endif( )
-    else( )
-        message( SEND_ERROR "clang-tidy requested but executable not found" )
-    endif( )
-endif( )
+	endif( )
+endfunction()
 
-if( ENABLE_INCLUDE_WHAT_YOU_USE )
-    find_program( INCLUDE_WHAT_YOU_USE include-what-you-use )
-    if( INCLUDE_WHAT_YOU_USE )
-        set( CMAKE_CXX_INCLUDE_WHAT_YOU_USE ${INCLUDE_WHAT_YOU_USE} )
-    else( )
-        message( SEND_ERROR "include-what-you-use requested but executable not found" )
-    endif( )
-endif( )
+function( target_try_enable_clang_tidy target )
+	if( ENABLE_CLANG_TIDY )
+		find_program( CLANGTIDY clang-tidy )
+		if( CLANGTIDY )
+			set( CLANG_TIDY_OPTIONS
+				 ${CLANGTIDY}
+				 -extra-arg=-Wno-unknown-warning-option
+				 -extra-arg=-Wno-ignored-optimization-argument
+				 -extra-arg=-Wno-unused-command-line-argument
+				 -p )
+			if( NOT "${CMAKE_CXX_STANDARD}" STREQUAL "" )
+				if( "${CLANG_TIDY_OPTIONS_DRIVER_MODE}" STREQUAL "cl" )
+					set( CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=/std:c++${CMAKE_CXX_STANDARD} )
+				else( )
+					set( CLANG_TIDY_OPTIONS ${CLANG_TIDY_OPTIONS} -extra-arg=-std=c++${CMAKE_CXX_STANDARD} )
+				endif( )
+			endif( )
+			set_property( TARGET ${target} PROPERTY CXX_CLANG_TIDY ${CLANG_TIDY_OPTIONS} )
+		else( )
+			message( SEND_ERROR "clang-tidy requested but executable not found" )
+		endif( )
+	endif( )
+endfunction()
+
+function( target_try_enable_include_what_you_use target )
+	if( ENABLE_INCLUDE_WHAT_YOU_USE )
+		find_program( INCLUDE_WHAT_YOU_USE include-what-you-use )
+		if( INCLUDE_WHAT_YOU_USE )
+			set_property( TARGET ${target} PROPERTY CXX_INCLUDE_WHAT_YOU_USE ${INCLUDE_WHAT_YOU_USE} )
+		else( )
+			message( SEND_ERROR "include-what-you-use requested but executable not found" )
+		endif( )
+	endif( )
+endfunction()
